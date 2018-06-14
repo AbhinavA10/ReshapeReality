fc5cb086859c7a19089ee1f6c67d5a5fd683b94a
Abhi <abhiraja1012@gmail.com>
2018-06-14 00:16:48 -0400

    Created Tilesets for levelMaps
    Created Tilesets to be used in tiled level making

---
+++ Game/data/Levels/DungeonSet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="DungeonSet" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="72" columns="8">
+ <image source="tileset2.jpg" width="288" height="320"/>
+</tileset>
---
+++ Game/data/Levels/scifi_platformTiles_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="scifi_platformTiles_32x32" tilewidth="32" tileheight="32" tilecount="805" columns="35">
+ <image source="scifi_platformTiles_32x32.png" width="1120" height="736"/>
+</tileset>
